943ed32d7c4dcb3fb2ae068539d343105ee5d589
Paul Salmon <psalmon88@gmail.com>
2014-09-24 03:18:34 -0400

    commit 3
    Should work. Not thoroughly tested, but seems complete.

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,55 +1,97 @@
-
 public class Knapsack {
 	
-	public static int[][] valueTable;
-	public static boolean[][] keepTable;
-	public static int items[];
-	public static int weights[];
+	static int[][] valueTable;
+	static boolean[][] keepTable;
+	static int values[];
+	static int weights[];
+	static int totalItems;
+	static int maxWeight;
 
 	public static void main(String[] args) {
-
-		int capacity = Integer.parseInt(args[0]);
-		int maxItems = Integer.parseInt(args[1]);
+		totalItems = Integer.parseInt(args[0]);
+		maxWeight = Integer.parseInt(args[1]);
 		
-		valueTable = new int[maxItems+1][maxItems+1];
-		keepTable = new boolean[maxItems+1][maxItems+1];
-		items = new int[maxItems+1];
-		weights = new int[maxItems+1];
+		values = new int[maxWeight+1];//rename to values?
+		weights = new int[maxWeight+1];
 		
-		initializeItems(capacity, maxItems);
-		buildTables(capacity, maxItems);
+		valueTable = new int[maxWeight+1][maxWeight+1];
+		keepTable = new boolean[maxWeight+1][maxWeight+1];
+		
+		initializeItems();
+		buildTables();
+		getMax();
 
 	}
 	
-	public static void initializeItems(int capacity, int maxItems){
-		items[0] = 0; weights[0] = 0;
+	public static void initializeItems(){
+		values[0] = 0; weights[0] = 0;
 				
-		for(int i = 1; i < maxItems; i++){
-			items[i] = (int) (Math.random()*100);
-			weights[i] = (int) (Math.random()*capacity);
+		for(int i = 1; i <= totalItems; i++){
+			values[i] = (int) (Math.random()*10);//value
+			System.out.print("Value: " + values[i] + "\t");
+			weights[i] = (int) (Math.random()*maxWeight);//weight
+			System.out.println("Weight: " + weights[i]);
+			//if(weights[i] == 0){weights[i] = 1;} //make sure nothing is weight 0
 		}
 	}
 	
 	
-	public static void buildTables(int capacity, int maxItems){
-		for(int i = 0; i < maxItems; i++){//initialize with 0s for weight of 0 and value of 0
-			valueTable[i][0] = 0;
-			keepTable[i][0] = false;
-			valueTable[0][i] = 0;
-			keepTable[0][i] = false;
-		}
+	public static void buildTables(){
 		
-		for(int i = 1; i < maxItems; i++){//weights
-			for(int j = 1; j < maxItems; j++){//item key
-				if(i > weights[j] && valueTable[i-(weights[j])][j-1] + items[j] >= valueTable[i][j-1]){//if it fits. if its value + remaining weight from prev row > prev item.
-					valueTable[i][j] = items[j] + valueTable[i-(weights[j])][j-1];//add the addition of the two
-					keepTable[i][j] = true;
-					}else{
-						valueTable[i][j] = valueTable[i][j-1];
-						keepTable[i][j] = false;
-					}
+		for(int i = 1; i < totalItems; i++){//items
+			for(int j = 1; j < maxWeight; j++){//weights
+				int prev = valueTable[i-1][j];
+				int curr = 0;
+				
+				if(weights[j] <= maxWeight){ 
+					if(j-(weights[i]) >= 0){ curr = valueTable[i-1][j-(weights[i])] + values[i]; }
+				}
+				
+				valueTable[i][j] = Math.max(prev,  curr);
+				
+				keepTable[i][j] = (curr > prev);
+				if(curr == 0) keepTable[i][j] = false;
 			}
 		}
+	}//buildTables
+	
+	public static void getMax(){
+		
+		//DEBUGGING
+		for(int i = 0; i < totalItems; i++){
+			for(int j = 0; j < maxWeight; j++){
+				System.out.print(valueTable[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 0; i < totalItems; i++){
+			for(int j = 0; j < maxWeight; j++){
+				System.out.print(keepTable[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		//DEBUGGING
+		
+		int j = maxWeight;
+		boolean[] take = new boolean[totalItems+1];
+		
+		for(int i = totalItems; i > 0; i--){
+			if(j-weights[i] > 0){ 
+				if(keepTable[i][j] = true){ take[i] = true; j = j-weights[i]; }
+				else				{ take[i] = false; }
+			}else break;
+		}
+		
+		System.out.println("Item" + "\t" + "Value" + "\t" + "Weight" + "\t" + "Take");
+		for (int i = 1; i <= totalItems; i++){
+			System.out.println(i + "\t" + values[i] + "\t" + weights[i] + "\t" + take[i]);
+		}
+		
 	}
 
 
---
+++ src/README.md
@@ -0,0 +1,8 @@
+Dynamic Programming solution to the common Knapsack problem.
+This is not thoroughly tested, though I believe it works.
+
+Usage:
+Command line arg[0] is the number of items you want to consider.
+Command line arg[1] is the maximum weight the knapsack can hold (whole number integer).
+
+Output is printed in table format for whether or not the item will be taken.
